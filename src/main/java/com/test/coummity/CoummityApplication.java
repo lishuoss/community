02aa4d75304bc20dadeb6c448038239c345f8e76
lishuo <1350159649@qq.com>
2020-05-30 19:40:59 +0800

    first

---
+++ src/main/java/com/test/coummity/CoummityApplication.java
@@ -0,0 +1,13 @@
+package com.test.coummity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoummityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoummityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/coummity/CoummityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.coummity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoummityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
